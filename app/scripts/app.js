076afb0871eaf3404e3fd9017a2cc081be25d564
Manu <bionikspoon@gmail.com>
2015-06-06 19:28:45 -0500

    setup

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,14 +2,14 @@
 
 /**
  * @ngdoc overview
- * @name garageCommerceApp
+ * @name GarageCommerceApp
  * @description
- * # garageCommerceApp
+ * # GarageCommerceApp
  *
  * Main module of the application.
  */
 angular
-  .module('garageCommerceApp', [
+  .module('GarageCommerceApp', [
     'ngAnimate',
     'ngAria',
     'ngCookies',
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,12 +2,12 @@
 
 /**
  * @ngdoc function
- * @name garageCommerceApp.controller:MainCtrl
+ * @name GarageCommerceApp.controller:MainCtrl
  * @description
  * # MainCtrl
- * Controller of the garageCommerceApp
+ * Controller of the GarageCommerceApp
  */
-angular.module('garageCommerceApp')
+angular.module('GarageCommerceApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
