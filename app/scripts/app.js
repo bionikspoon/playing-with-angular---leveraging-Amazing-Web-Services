5c16dbff4152ad1db549339ee9be10825456faaa
Manu <bionikspoon@gmail.com>
2015-06-06 19:24:01 -0500

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name garageCommerceApp
+ * @description
+ * # garageCommerceApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('garageCommerceApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name garageCommerceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the garageCommerceApp
+ */
+angular.module('garageCommerceApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
